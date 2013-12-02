60943c30a60960b69566afeaceb3b13c5b831917
btgarber <mrbrentgarber@gmail.com>
2013-12-02 14:33:06 -0700

    Interface and Lessons added

--- src/_lesson.py
+++ src/_lesson.py
@@ -4,36 +4,42 @@ Lesson text is loaded from html files in lessons/. Main program will just
 call get_lessons to get a list of all lessons that the interface should
 present. """
 
+import os
+import _interface
+
 class Lesson:
     """ Basically just a struct of lesson related data. """
 
-    def __init__(self, setup_and_loop, text):
+    def __init__(self, number):
         """ The setup_and_loop flag lets the interface know wether setup and
         loop text boxes should be shown. Notice that in lesson one there is
         only one text box presented to the user and it is implicitly a loop
         box. Setup is introduce later. The text is just the lesson text. """
-        self.setup_and_loop = setup_and_loop
-        self.text = text
-        # any other data needed?
- 
 
-def _get_text(fname):
-    """ Get lesson text from file with name fname in lesson_dir`. """
-    pass
+        self.lesson_id = number
+
+        # read saved data from a file and store here
+        self.saved_data = []
+
+        # load the template for the page
+        self.page = _interface.ReadFile("lessons/lesson"+str(self.lesson_id)+".html")
+        
+    def GetPageSource(self):
+        # insert saved data here.
+
+        page = self.page.replace('{SAVE}','')
+        
+        return page
+
+        
 
 def get_lessons(lesson_dir):
     """ Add lessons to lessons and return. """
-    lessons = []
-    lessons.append(Lesson(False,
-                          _get_text(lesson_dir + 'lesson1.html'))) # lesson 01
-    # lesson 02
-    # lesson 03
-    # lesson 04
-    # lesson 05
-    # lesson 06
-    # lesson 08
-    # lesson 09
-    # lesson 10
+    lessons = {}
+    
+    for index in range(0, 9):
+        lessons[index] = (Lesson(index))
+
     return lessons
 
 if __name__ == '__main__':
